ee349d2b4568eb48df06132e78dc2c44bbe6a636
Adrian424-sudo <147409297+Adrian424-sudo@users.noreply.github.com>
2026-09-18 18:15:02 +0000

    ✨ Feat: Enhance app.js Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include itgam image

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
+//Funcion para manejar errores
 var createError = require('http-errors');
+//Importa el framework express
 var express = require('express');
+//Importa modulos para manejar rutas
 var path = require('path');
+//Importa modulos para manejar cookies
 var cookieParser = require('cookie-parser');
+//Importa modulos para manejar logs
 var logger = require('morgan');
 
+//Importan las rutas de la aplicacion
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Crea la aplicacion express
 var app = express();
 
-// view engine setup
+// Configura el motor de vistas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Configura middlewares de la aplicacion
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Configura la carpeta de archivos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Registramos rutas de la aplicacion
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -27,7 +38,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manejador de errores
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
